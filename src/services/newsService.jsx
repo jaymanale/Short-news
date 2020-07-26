1a9c05268da1d06734d185eaeda055ad7146eea1
Jayram Manale <jaymanale29@gmail.com>
2020-07-26 13:40:56 +0530

    Hiding API Keys

--- src/services/newsService.jsx
+++ src/services/newsService.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-//`https://gnews.io/api/v3/topics/world?country=in&max=50&token=67780986506c8922cf308108079529fd`
+
 export const getTopHeadlines = async (category) => {
   console.log('Category:', category);
   let response = await axios.get(
-    `https://gnews.io/api/v3/top-news?country=in&token=67780986506c8922cf308108079529fd`
+    `https://gnews.io/api/v3/top-news?country=in&token=${process.env.REACT_APP_TOKEN}`
   );
   console.log(response);
   return response.data;
@@ -13,22 +13,9 @@ export const getTopHeadlines = async (category) => {
 export const getCategoryNews = async (category) => {
   console.log('Category:', category);
   let response = await axios.get(
-    `https://gnews.io/api/v3/topics/${category}?country=in&token=67780986506c8922cf308108079529fd`
+    `https://gnews.io/api/v3/topics/${category}?country=in&token=${process.env.REACT_APP_TOKEN}`
   );
   console.log(response);
   return response.data;
 };
 
-export const getTechnology = async () => {
-  let response = await axios.get(
-    'https://newsapi.org/v2/everything?q=technology&apiKey=d6ce7b4875084b0faaa665b61ed31ad3'
-  );
-  return response.data.articles;
-};
-
-export const getBusiness = async () => {
-  let response = await axios.get(
-    'https://newsapi.org/v2/top-headlines?q=business&apiKey=d6ce7b4875084b0faaa665b61ed31ad3'
-  );
-  return response.data.articles;
-};
