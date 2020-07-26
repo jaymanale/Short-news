6f73ac88b308c39d4264c13fb4284decd1ea1077
Jayram Manale <jaymanale29@gmail.com>
2020-07-26 13:48:47 +0530

    removed console.log

--- src/services/newsService.jsx
+++ src/services/newsService.jsx
@@ -2,20 +2,20 @@ import axios from 'axios';
 
 
 export const getTopHeadlines = async (category) => {
-  console.log('Category:', category);
+  
   let response = await axios.get(
     `https://gnews.io/api/v3/top-news?country=in&token=${process.env.REACT_APP_TOKEN}`
   );
-  console.log(response);
+  
   return response.data;
 };
 
 export const getCategoryNews = async (category) => {
-  console.log('Category:', category);
+  
   let response = await axios.get(
     `https://gnews.io/api/v3/topics/${category}?country=in&token=${process.env.REACT_APP_TOKEN}`
   );
-  console.log(response);
+  
   return response.data;
 };
 
