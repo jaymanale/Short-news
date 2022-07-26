84f67d4f287fa5f3df7464c8238595752c506883
jaymanale <jaymanale29@gmail.com>
2022-07-26 10:51:35 +0530

    API Key hide change

--- src/services/newsService.jsx
+++ src/services/newsService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getTopHeadlines = async (category) => {
   let response = await axios.get(
-    `https://gnews.io/api/v4/top-headlines?lang=en&token=67780986506c8922cf308108079529fd`
+    `https://gnews.io/api/v4/top-headlines?lang=en&token=${process.env.REACT_APP_TOKEN}`
   );
 
   return response.data;
@@ -10,7 +10,7 @@ export const getTopHeadlines = async (category) => {
 
 export const getCategoryNews = async (category) => {
   let response = await axios.get(
-    `https://gnews.io/api/v4/top-headlines?topic=${category}&lang=en&token=67780986506c8922cf308108079529fd`
+    `https://gnews.io/api/v4/top-headlines?topic=${category}&lang=en&token=${process.env.REACT_APP_TOKEN}`
   );
 
   return response.data;
